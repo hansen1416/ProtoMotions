0de3b25cf320042b34d7a7a1044703cfd29b6c0c
hansen1416 <L.Han2@uni.brighton.ac.uk>
2026-05-13 16:54:46 +0100

    1

--- note/train_hhi_single.sh
+++ note/train_hhi_single.sh
@@ -9,20 +9,28 @@ python protomotions/train_agent.py \
     --experiment-path examples/experiments/mimic/mlp.py \
     --experiment-name hhi_single_male_0e26b88d_lowmem \
     --motion-file /home/hlz/datasets/humos_proto_motionlib/humos_1.pt \
-    --num-envs 2 \
-    --batch-size 2 \
+    --num-envs 16 \
+    --batch-size 32 \
     --headless True
 
+python protomotions/train_agent.py \
+    --robot-name smpl \
+    --simulator isaacgym \
+    --experiment-path examples/experiments/mimic/mlp.py \
+    --experiment-name hhi_single_male_0e26b88d_lowmem \
+    --motion-file /home/hlz/datasets/humos_proto_motionlib/humos_1.pt \
+    --num-envs 16 \
+    --batch-size 32 \
+    --headless True
 
 python protomotions/train_agent.py \
     --robot-name hhi_smpl_single \
     --simulator isaacgym \
     --experiment-path examples/experiments/mimic/mlp.py \
     --experiment-name hhi_single_male_0e26b88d_cloud_smoke \
-    --motion-file /home/hlz/datasets/humos_proto_motionlib/humos_1.pt \
-    --num-envs 2 \
-    --batch-size 2 \
-    --training-max-steps 100000 \
+    --motion-file ./humos_1.pt \
+    --num-envs 16 \
+    --batch-size 128 \
     --use-wandb \
     --wandb-project hhi-protomotions \
     --wandb-entity yugoamaryl \
@@ -30,4 +38,29 @@ python protomotions/train_agent.py \
     --wandb-tags local lowmem single_motion male_0e26b88d \
     --headless True
 
-# --num-envs 4096 --batch-size 16384
+# git clone -b feature/hhi https://github.com/hansen1416/ProtoMotions.git
+
+# pip install gdown
+# gdown 1eDoIKUGs8VYXQY1RJskSEJk5WSLUBNhN
+
+# pip install -e .
+
+# wandb login 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
+
+
+# apt update && apt install -y tmux
+# tmux new -s hhi
+# tmux kill-session -t hhi
+
+
+
+# wandb login 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
+
+
+
+python data/scripts/convert_amass_to_proto.py \
+    /home/hlz/datasets/amass_data/ \
+    --humanoid-type smpl \
+    --output-fps 30 \
+    --motion-config /home/hlz/datasets/wb13.yaml \
+    --force-remake
